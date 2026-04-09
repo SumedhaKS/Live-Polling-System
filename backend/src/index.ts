000207aafb81a5c7a787fa1ce9c635376185e678
SumedhaKS <kssumedha24@gmail.com>
2026-04-09 17:30:50 +0530

    added votes api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import express from "express";
 import cors from "cors";
 import pollRoutes from "./routes/poll.routes";
 import authRoutes from "./routes/auth.routes";
+import voteRoutes from "./routes/vote.routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,5 +15,6 @@ app.use(express.json())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/v1/polls", pollRoutes);
+app.use("/api/v1/votes", voteRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
