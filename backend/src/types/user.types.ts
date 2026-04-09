d25c61b7373b1a3c7a65bfd8792cdd6ff7850d95
SumedhaKS <kssumedha24@gmail.com>
2026-04-09 22:57:05 +0530

    updated

--- backend/src/types/user.types.ts
+++ backend/src/types/user.types.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const userSignupSchema = z.object({
-    name: z.string().min(4).optional(),
+    name: z.string().optional(),
     email: z.email(),
     password: z.string().min(4).max(15)
 })
