c94d0c9a0d7b8418e4a96c525ff200b192b7317b
SumedhaKS <kssumedha24@gmail.com>
2026-05-03 17:52:47 +0530

    updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/polls", pollRoutes);
 app.use("/api/v1/votes", voteRoutes);
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
+import Poll from "./pages/Poll"
 function App() {
 
   return (
@@ -8,7 +10,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Navigate to="/login " />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="/poll/:shareCode" element={<Poll />} />
       </Routes>
     </BrowserRouter>
   )
